113a74978670acceffcacecc3d7e7ed7c86268cf
Özgür Işık Çimen <50545611+26518304542@users.noreply.github.com>
2022-11-29 19:33:50 +0300

    Add files via upload

---
+++ TodoItem.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Blazorapp
+{
+    public class TodoItem
+    {
+        public string Title { get; set; }
+
+        public DateTime DueDate { get; set; }
+
+        public Boolean IsDone { get; set; } = false;
+    }
+}
